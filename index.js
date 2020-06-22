2ff2baf8c603bcf83279096d94998c28a79929e7
unknown <mdm317@naver.com>
2020-06-22 17:48:55 +0900

    pug

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import { router } from "./router.js";
+
+const PORT = 4000;
+const app = express();
+
+app.set("view engine", "pug");
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(helmet());
+
+app.use('/', router);
+
+app.listen(PORT);
+
+export default app;
