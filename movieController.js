295b2560f9b0be4e358fa03ae1396ef30d123629
unknown <mdm317@naver.com>
2020-06-29 14:51:45 +0900

    2

--- movieController.js
+++ movieController.js
@@ -6,47 +6,52 @@ import {
   } from "./db.js";
   
   export const home = (req, res) => {
-      const videos = getMovies();
+    const videos = getMovies();
     //console.log(videos[0]);
-      res.render("movies", {
-        videos,
-        title: "Movies!",
-        pageTitle: "Movies!",
+    res.render("movies", {
+      videos,
+      title: "Movies!",
+      pageTitle: "Movies!"
     });
   };
   export const movieDetail = (req, res) => {
-      const {params:{id}} = req;
-      const movie  = getMovieById(id);
-      if(movie){
-        res.render("detail", {
-            movie,
-            title:movie.title
-        });
-      }else{
-        res.render("404");
-      }
-      
+    const {
+      params: { id }
+    } = req;
+    const movie = getMovieById(id);
+    if (movie) {
+      res.render("detail", {
+        movie,
+        title: movie.title
+      });
+    } else {
+      res.status(404);
+      res.render("404");
+    }
   };
-export const filterMovie = (req, res) => {
+  export const filterMovie = (req, res) => {
     console.log(req.query);
-    const {query:{rating, year}} = req;
+    const {
+      query: { rating, year }
+    } = req;
     console.log(rating, year);
-    if(rating !== undefined){
-        const videos = getMovieByMinimumRating(rating);
-        res.render('movies',{
-            videos,
-            pageTitle:`Searching by rating: ${rating}`,
-            title:"Movies!"
-        })
+    if (rating !== undefined) {
+      const videos = getMovieByMinimumRating(rating);
+      res.render("movies", {
+        videos,
+        pageTitle: `Searching by rating: ${rating}`,
+        title: "Movies!"
+      });
     }
-    if(year!== undefined){
-        const videos = getMovieByMinimumYear(year);
-        res.render('movies',{
-            videos,
-            pageTitle:`Searching by year: ${year}`,
-            title:"Movies!"
-        })
+    if (year !== undefined) {
+      const videos = getMovieByMinimumYear(year);
+      res.render("movies", {
+        videos,
+        pageTitle: `Searching by year: ${year}`,
+        title: "Movies!"
+      });
     }
-    
-};
+    res.status(404);
+    res.render("404");
+  };
   
