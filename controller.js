8662f4fdfe694964fb0825f69af982bc7625b2b8
unknown <mdm317@naver.com>
2020-06-29 16:10:59 +0900

    pug

--- controller.js
+++
@@ -1,6 +0,0 @@
-export const home = (req,res)=>res.render('home',{title:"Home"});
-export const login = (req,res)=>res.render('login',{title:"Login"});
-export const photos = (req,res)=>res.render('photos',{title:"photos"});
-export const profile = (req,res)=>res.render('profile',{title:"profile"});
-
-
---
+++ routers/globalRouter.js
@@ -0,0 +1,24 @@
+import express from "express";
+import routes from "../routes.js";
+import { home, search } from "../controllers/videoController.js";
+import {
+  getJoin,
+  getLogin,
+  logout,
+  postJoin,
+  postLogin
+} from "../controllers/userController.js";
+
+const globalRouter = express.Router();
+
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
+globalRouter.get(routes.home, home);
+globalRouter.get(routes.search, search);
+globalRouter.get(routes.logout, logout);
+
+export default globalRouter;
