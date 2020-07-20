80a5346fb9f58da58c95ffea3278f260314a3a45
unknown <mdm317@naver.com>
2020-07-20 16:54:21 +0900

    'c'

--- src/index.js
+++ src/index.js
@@ -1,24 +1,40 @@
-const indicator = document.querySelector(".indicator");
-const API_URL = "http://ip-api.com/json/";
-const callUrl = async () => {
-  try {
-    indicator.innerHTML = "Finding Location...";
-    const data = await fetch(API_URL, {
-      headers: {
-        "Content-Type": "application/json"
-      },
-      method: "GET"
-    });
-    const parseData = await data.json();
-    const { country, region, isp } = parseData;
-    indicator.innerHTML = `your Location === country: ${country} region=${region} isp=${isp}`;
 
-    console.log(country, region, isp);
-  } catch (error) {
-    console.log(error);
-  }
+
+const API_URL = "https://api.coinpaprika.com/v1/tickers";
+
+const coinPrices = document.querySelector('.coinPrices');
+const loading = document.querySelector('.loading');
+const coinPrice = (name,price)=>{
+  const li = document.createElement('li');
+  const text = document.createTextNode( `${name} price is === ${price}` );
+  li.appendChild(text);
+  return li;
+}
+const getCoins= async()=>{
+  loading.innerHTML="Prices Is Loading----";
+
+  const fetchData = await fetch(API_URL);
+  const data = await fetchData.json();
+  //console.log(data);
+  //loading 중
+
+  loading.innerHTML="";
+
+  coinPrices.innerHTML="";
+  data.forEach(coin => {
+    const {name, quotes:{USD:{price}}}=coin;
+    coinPrices.appendChild(coinPrice(name,price));
+
+  });
+
+
+}
+
+const init= async()=>{
+  setInterval(getCoins,5000);
+
+
+
 };
-const init = () => {
-  document.addEventListener("DOMContentLoaded", callUrl);
-};
+
 init();
